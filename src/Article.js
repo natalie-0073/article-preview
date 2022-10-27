d2bb09ad562adf74698967c67f0cc72c7b2d0f4f
Nataliia Chala <hatalochka.rv@gmail.com>
2022-10-27 21:43:37 +0100

    finished styling and added some interaction

--- src/Article.js
+++ src/Article.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import './_article.scss';
 import avatar from './images/avatar-michelle.jpg';
 import share from './images/icon-share.svg';
+import fb from './images/icon-facebook.svg';
+import pin from './images/icon-pinterest.svg';
+import tw from './images/icon-twitter.svg';
 export default function Article(){
+    
+    function handleClick(event){
+        event.currentTarget.classList.toggle('clicked');
+    }
     return(
-        // <main>
+        
+        <div className="content">
             <div className="container">
-                {/* <div className="content"> */}
+              
                     <div className="row">
                         <div className="col-sm-5 content__picture"></div>
                         <div className="col-sm-7 content__text">
@@ -18,12 +26,25 @@ export default function Article(){
                                 <p className="content__contact-info--name">Michelle Appleton</p>
                                 <p className="content__contact-info--date">28 Jun 2020</p>
                             </div>
-                            <div className="content__contacts-share"><img src={share} alt="share icon"/></div>
+                            <div className="content__contacts-share" onClick={handleClick}>
+                                
+                                <img src={share} alt="share icon"/>
+                            <div className="triangle"></div>
+                            <div className="share-option">
+                            <span>SHARE</span>
+                            <img src={fb} alt="share"/>
+                            
+                            <img src={tw} alt="share"/>
+                            <img src={pin} alt="share"/>
+                            </div>
+                            </div>
+                            
                         </div>
                         </div>
                     </div>
-                    {/* </div> */}
+                    
             </div>
-        // </main>
+            </div>
+        
     );
 }
